7598d76888da3b043c09aba67bacb4d002a39c09
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-07-19 21:30:32 +0900

    Pointer 생성

--- new-blog/src/components/NavBar.js
+++ new-blog/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const NavBar = () => {
     return (
@@ -7,7 +7,12 @@ const NavBar = () => {
                 <Link className="navbar-brand" to="/">Home</Link>
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" to="/blogs">Blogs</Link>
+                        <NavLink
+                            activeClassName="active"
+                            className="nav-link"
+                            aria-current="page"
+                            to="/blogs">Blogs
+                        </NavLink>
                     </li>
                 </ul>
             </div>
--- new-blog/src/pages/ListPage.js
+++ new-blog/src/pages/ListPage.js
@@ -1,6 +1,42 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Card from '../components/Card';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
+
 const ListPage = () => {
+    const history = useHistory();
+    const [posts, setPosts] = useState([])
+
+    const getPosts = () => {
+        axios.get('http://localhost:3001/posts').then((res) => {
+            // console.log(res.data);
+            setPosts(res.data)
+        })
+    }
+    useEffect(() => {
+        getPosts();
+    }, []);
+
     return (
-        <div>List Page</div>
-    );
+        <div>
+            <div className='d-flex justify-content-between'>
+                <h1>Blogs</h1>
+                <div>
+                    <Link to="/blogs/create" className='btn btn-success'>
+                        Create New
+                    </Link>
+                </div>
+            </div>
+            {posts.map(post => {
+                return (
+                    <Card
+                        key={post.id}
+                        title={post.title}
+                        onClick={() => history.push('/blogs/edit')} />
+                )
+            })}
+        </div>
+    )
 };
 export default ListPage

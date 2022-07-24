96a01236246381a32c5f80d41c2f6f59c97f53a9
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-07-24 15:13:55 +0900

    Edit Route 추가

--- new-blog/src/pages/ShowPage.js
+++ new-blog/src/pages/ShowPage.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import LoadingSpinner from '../components/LoadingSpinner';
+import { Link } from 'react-router-dom';
 
 const ShowPage = () => {
     const { id } = useParams();
@@ -29,7 +30,16 @@ const ShowPage = () => {
 
     return (
         <div>
-            <h1>{post.title}</h1>
+            <div className='d-flex'>
+                <h1 className='flex-grow-1'>{post.title}</h1>
+                <div>
+                    <Link className='btn btn-primary'
+                        to={`blogs/${id}/edit`}
+                    >
+                        Edit
+                    </Link>
+                </div>
+            </div>
             <small className='text-muted'>
                 Created At: {printDate(post.createdAt)}
             </small>
